e8cb25c72945086ed0cb0066c6f3f0968cb00aee
Mong-Gu <yhw8258@naver.com>
2021-03-25 13:45:49 +0900

    dasd

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ cp ../get_next_line.h .
 cat run.sh | sed -i .backup "s/echo/echo -e/g" run.sh
 ./run.sh
 
-sleep .5
+sleep 2
 echo -e "\n\033[1;31m******************** Test 2: 42TESTERS-GNL ********************"
 echo -e "\033[1;33m******** ref: https://github.com/Mazoise/42TESTERS-GNL ********\033[0m\n"
 sleep 1
@@ -22,7 +22,7 @@ git clone https://github.com/Mazoise/42TESTERS-GNL.git 42TESTERS-GNL
 cd 42TESTERS-GNL
 bash all_tests_with_bonus.sh
 
-slepp .5
+sleep 2
 echo -e "\n\033[1;31m******************** Test 3: 42cursus_gnl_tests ********************"
 echo -e "\033[1;33m********* ref: https://github.com/mrjvs/42cursus_gnl_tests *********\033[0m\n"
 sleep 1
@@ -32,7 +32,7 @@ cd 42cursus_gnl_test
 cat Makefile | sed -i .backup "s/\.\/get_next_line/\.\.\//g" Makefile
 make bonus
 
-slepp .5
+sleep 2
 echo -e "\n\033[1;31m******************** Test 4: GNL_lover ********************"
 echo -e "\033[1;33m******* ref: https://github.com/charMstr/GNL_lover ********\033[0m\n"
 sleep 1
@@ -41,4 +41,3 @@ git clone https://github.com/charMstr/GNL_lover.git GNL_lover
 cd GNL_lover
 cp ../get_next_line* ./copy_in_here_GNL_files
 ./GNL_lover.sh
-
