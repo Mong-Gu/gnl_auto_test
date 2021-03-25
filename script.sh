02e8499aebf2993cdcc19c6c9c9e38b92089a562
Mong-Gu <yhw8258@naver.com>
2021-03-25 13:29:34 +0900

    dasd

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 cp ../gnl/* .
 
 echo -e "\n\033[1;31m******************** Test 1: gnl killer ********************"
-echo -e "\n\033[1;33m****** ref: https://github.com/DontBreakAlex/gnlkiller *****\033[0m\n"
+echo -e "\033[1;33m****** ref: https://github.com/DontBreakAlex/gnlkiller *****\033[0m\n"
 sleep 1
 git clone https://github.com/DontBreakAlex/gnlkiller.git gnlkiller
 cd gnlkiller
@@ -15,7 +15,7 @@ cat run.sh | sed -i .backup "s/echo/echo -e/g" run.sh
 
 sleep .5
 echo -e "\n\033[1;31m******************** Test 2: 42TESTERS-GNL ********************"
-echo -e "\n\033[1;33m******** ref: https://github.com/Mazoise/42TESTERS-GNL ********\033[0m\n"
+echo -e "\033[1;33m******** ref: https://github.com/Mazoise/42TESTERS-GNL ********\033[0m\n"
 sleep 1
 cd ..
 git clone https://github.com/Mazoise/42TESTERS-GNL.git 42TESTERS-GNL
@@ -24,10 +24,10 @@ bash all_tests_with_bonus.sh
 
 slepp .5
 echo -e "\n\033[1;31m******************** Test 3: 42cursus_gnl_tests ********************"
-echo -e "\n\033[1;33m********* ref: https://github.com/mrjvs/42cursus_gnl_tests *********\033[0m\n"
+echo -e "\033[1;33m********* ref: https://github.com/mrjvs/42cursus_gnl_tests *********\033[0m\n"
 sleep 1
 cd ..
 git clone https://github.com/mrjvs/42cursus_gnl_tests.git 42cursus_gnl_test
 cd 42cursus_gnl_test
-cat Makefile | sed -i .backup "s/\./get_next_line/\.\.//g" Makefile
+cat Makefile | sed -i .backup "s/\.\/get_next_line/\.\.\//g" Makefile
 make bonus
